c4a2e40eb8a6fe24a6f24d36584ebda9647a64fa
1784950 <1784950@TE-GDA-D-D36G10>
2024-02-28 15:16:39 +0100

    CCTV camera working

---
+++ Assets/Scripts/cctvcamera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cctvcamera : MonoBehaviour
+{
+
+    Transform cameraLens;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform cameraPosition = transform.Find("Camera");
+        cameraLens = cameraPosition.Find("Cylinder");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0, Mathf.PingPong(Time.time, 9) * 10 - 45, 0));
+        checkSight();
+    }
+
+    void checkSight()
+    {
+        Debug.DrawRay(cameraLens.position, cameraLens.TransformDirection(Vector3.down) * 100, Color.yellow);
+    }
+}
