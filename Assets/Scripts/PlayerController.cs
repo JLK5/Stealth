970e6c3a50ed0a5ce1d202c15eccd249d07d00f6
1784950 <1784950@TE-GDA-D-D36G10>
2024-04-10 15:13:57 +0200

    Prefabs + Level 1 model started, fixed jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,62 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //silnik fizyczny dla obiektu gracza
     Rigidbody rb;
-    //si³a skoku
     public Vector3 jump;
     public bool isGrounded;
     public float jumpForce = 1f;
 
     public float moveSpeed = 5f;
-    // Start is called before the first frame update
     void Start()
     {
-        //przypnij rigidbody gracza do zmiennej rb
         rb = GetComponent<Rigidbody>();
         jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        //transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
-        //mo¿na proœciej: Vector3.right
 
-        //zczytaj klawiaturê w osi poziomej:
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-        //wyœwietl w konsoli stan klawiatury
-        //Debug.Log(horizontalInput);
-
-        //wylicz przesuniêcie w osi x
         Vector3 movement = Vector3.right * horizontalInput;
 
-        //zczytaj z klawiatury oœ y
         float verticalInput = Input.GetAxisRaw("Vertical");
 
-        //wylicz przesuniêcie w osi y i dodaj do istniej¹cego przesuniêcia w osi x
         movement += Vector3.forward * verticalInput;
-
-        //normalizujemy wektor
         movement = movement.normalized;
-        //poprawka na czas od ostatniej klatki
         movement *= Time.deltaTime;
-        //pomnó¿ przez prêdkoœæ ruchu
         movement *= moveSpeed;
-
-        //przesuñ gracza w osi x
-        //transform.position += movement;
-        
-        //próbujemy u¿yc translate zamiast dodawac wspó³rzêdne
         transform.Translate(movement);
 
         
     }
-    void OnCollisionEnter()
+    void OnCollisionEnter(Collision collision)
     {
-        isGrounded = true;
+        if (collision.collider.transform.CompareTag("Ground"))
+            isGrounded = true;
     }
-    //spróbujmy obejœæ problem z opóŸnieniem wejœcia poprzez przeniesienie go do update
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
